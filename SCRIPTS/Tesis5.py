ce06f89a9fd6c1c7e801a42fb8432e72f057dfed
SDV244 <104800190+SDV244@users.noreply.github.com>
2023-01-20 13:48:19 +0000

    Incluida en el script parte para hacer trim de los audios pero no estoy seguro de si está bien, no logro localizar los archivos y me tocó quitar un monton de NaN que aparecian en el csv para que corriera

--- SCRIPTS/Tesis5.py
+++ SCRIPTS/Tesis5.py
@@ -34,7 +34,7 @@ target_fs = 24000  # target fs of project
 wl = 5  # Window length for formated rois
 path_annot = "../ANNOTATIONS_INTC41/INCT41/"  # location of bbox annotations
 path_audio = "../AUDIO_INTC41/INCT41/"  # location of raw audio
-path_save = "../SCRIPTS/TDS"  # location to save new samples
+path_save = "../SCRIPTS/TDS/"  # location to save new samples
 
 
 
@@ -59,7 +59,7 @@ for i in range (len(nombre)):
         
         
 # Select vocalizations of a single species   
-    df_rois = df.loc[(df.label=='BOAALB_M')|(df.label=='BOAALB_F') |(df.label=='BOALUN_F')| (df.label=='BOALUN_M')|(df.label=='PHYCUV_M')|(df.label=='PHUCUV_F')|(df.label=='DENCRU_M')|(df.label=='DENCRU_F')|(df.label=='PITAZU_F')|(df.label=='PITAZU_M'), :]    
+    df_rois = df.loc[(df.label=='BOAALB_M')|(df.label=='BOAALB_F'), :]# |(df.label=='BOALUN_F') | (df.label=='BOALUN_M')|(df.label=='PHYCUV_M')|(df.label=='PHUCUV_F')|(df.label=='DENCRU_M')|(df.label=='DENCRU_F')|(df.label=='PITAZU_F')|(df.label=='PITAZU_M'), :]    
 
    
 # format rois to a fixed window
@@ -68,11 +68,12 @@ for i in range (len(nombre)):
         roi_fmt = roi2windowed(wl, roi)
         rois_fmt = pd.concat([rois_fmt,roi_fmt],ignore_index=True)
         #rois_fmt = rois_fmt.append(roi_fmt)
-    
+        
     rois_fmt.reset_index(inplace=True, drop=True)
     #print(rois_fmt)
+    rois_fmt=rois_fmt.dropna(subset=['fname'])
     df_filt=(df[df["max_t"] <= 5])
-
+    
     #MultiLabel Window Cutting
     for x in range(0, 60 ,5):
             print (x)
@@ -100,17 +101,44 @@ print(v['label'].value_counts())
 ###########
 #%% Load audio, resample, trim, normalize and write to disk
 #for idx_row, roi_fmt in rois_fmt.iterrows():
-    #print(idx_row+1, '/', len(rois_fmt))
-    #full_path_audio = find_file(roi_fmt.fname, path_audio)[0]
-    #fname_audio = os.path.join(full_path_audio)
-    #s, fs = sound.load(fname_audio)
-    #s = sound.resample(s, fs, target_fs)
-    #s_trim = sound.trim(s, target_fs, roi_fmt.min_t, roi_fmt.max_t, pad=True)
+ #   print(idx_row+1, '/', len(rois_fmt))
+ #   full_path_audio = find_file(roi_fmt.fname, path_audio)[0]
+ #   fname_audio = os.path.join(full_path_audio)
+ #   s, fs = sound.load(fname_audio)
+ #   s = sound.resample(s, fs, target_fs)
+ #   s_trim = sound.trim(s, target_fs, roi_fmt.min_t, roi_fmt.max_t, pad=True)
 
-    #fname_save = os.path.join(path_save, roi_fmt.label+'_'+str(idx_row).zfill(3)) 
-    #sound.write(fname_save+'.wav', fs=target_fs, data=s_trim)
+ #   fname_save = os.path.join(path_save, roi_fmt.label+'_'+str(idx_row).zfill(3)) 
+ #   sound.write(fname_save+'.wav', fs=target_fs, data=s_trim)
 
-# save data frame
-#rois_fmt['fname_trim'] = rois_fmt.label + '_' + rois_fmt.index.astype(str).str.zfill(3)
-#rois_fmt.to_csv(path_save+'rois_details.csv', index=False)
+#%% save data frame
+rois_fmt['fname_trim'] = rois_fmt.label + '_' + rois_fmt.index.astype(str).str.zfill(3)
+rois_fmt.to_csv(path_save+'rois_details.csv', index=False)
 
+
+# %%
+import pandas as pd
+from pydub import AudioSegment
+
+# Read the dataframe
+df = pd.read_csv('../SCRIPTS/TDS/rois_details.csv')
+
+# Iterate through the dataframe
+for index, row in df.iterrows():
+    # Get the path of the audio file
+    audio_path = "../AUDIO_INTC41/INCT41/"
+    # Get the start and end time for trimming
+    start_time = row['min_t']
+    end_time = row['max_t']
+    name = row['fname_trim']
+    f_name = row["fname"]
+    
+    # Load the audio file
+    audio = AudioSegment.from_wav(audio_path + f_name)
+    # Trim the audio
+    audio_trimmed = audio[start_time:end_time]
+    # Save the trimmed audio
+    audio_trimmed.export(path_save + name, format="wav")
+
+print("All audio files have been trimmed successfully!")
+# %%
