129b3a02e88d478498c1c1852e2aeaaad93a33b7
SDV244 <104800190+SDV244@users.noreply.github.com>
2023-01-20 20:13:05 +0000

    archivos nuevos csv y estructura de archivos para que todo esté ordenado

--- SCRIPTS/Tesis5.py
+++ SCRIPTS/Tesis5.py
@@ -41,8 +41,22 @@ path_save = "../SCRIPTS/TDS/"  # location to save new samples
 
 nombre = next(walk('/workspaces/TESIS-VScode/ANNOTATIONS_INTC41/INCT41'), (None, None, []))[2]  # [] if no file
 anotate = next(walk('/workspaces/TESIS-VScode/ANNOTATIONS_INTC41/INCT41'), (None, None, []))[2]
-df_mlabel= []
+
 df = pd.DataFrame()
+
+def filter_window(df, start, end, step):
+        df_mlabel = []
+        for x in range(start, end, step):
+            df_windowed = df[(df['min_t'] >= x) & (df['max_t'] <= x + step)]
+            names = df_windowed['label']
+            df_mlabel.append(df_windowed)
+        return df_mlabel
+    
+
+def filter_label(row):
+    return row['label'] in ['PHYCUV_M', 'PHYCUV_F']#'BOAALB_M', 'BOAALB_F', 'BOALUN_F', 'BOALUN_M', 'PHYCUV_M', 'PHYCUV_F']
+
+    
 
 #Load multiple annotations from directory
 for i in range (len(nombre)): 
@@ -56,37 +70,11 @@ for i in range (len(nombre)):
         #df = df.append(df_aux)
         df.reset_index(inplace=True, drop=True)
         print(df)
-        
-        
-# Select vocalizations of a single species   
-    df_rois = df.loc[(df.label=='BOAALB_M')|(df.label=='BOAALB_F'), :]# |(df.label=='BOALUN_F') | (df.label=='BOALUN_M')|(df.label=='PHYCUV_M')|(df.label=='PHUCUV_F')|(df.label=='DENCRU_M')|(df.label=='DENCRU_F')|(df.label=='PITAZU_F')|(df.label=='PITAZU_M'), :]    
+       #MultiLabel Window Cutting
+        df_mlabel = filter_window(df, 0, 60, 5)
+ 
+print("Done!")    
 
-   
-# format rois to a fixed window
-    rois_fmt = pd.DataFrame()
-    for idx, roi in df_rois.iterrows():
-        roi_fmt = roi2windowed(wl, roi)
-        rois_fmt = pd.concat([rois_fmt,roi_fmt],ignore_index=True)
-        #rois_fmt = rois_fmt.append(roi_fmt)
-        
-    rois_fmt.reset_index(inplace=True, drop=True)
-    #print(rois_fmt)
-    rois_fmt=rois_fmt.dropna(subset=['fname'])
-    df_filt=(df[df["max_t"] <= 5])
-    
-    #MultiLabel Window Cutting
-    for x in range(0, 60 ,5):
-            print (x)
-            df_windowed = df[(df['min_t'] >= x) & (df['max_t'] <= x+5)]
-            names = df_windowed['label']
-            df_mlabel.append(df_windowed)
-          #  print ('-------')
-          #  print (names)
-           # print(df_windowed)
-        #Cada posición de la lista corresponde a una ventana de datos segmentados de acuerdo a wl comprendido en dataframes
-        #Each position on mlabel list correspond to a data segmented window comprehended in dataframes 
-    #print(df_mlabel)   
-print("Done!")       
 
 #%% Merging mlabel list of dataframes to a single dataframe
 v = pd.DataFrame()
@@ -95,31 +83,39 @@ v = pd.concat(df_mlabel,ignore_index=True)
    # v=v.append(df_mlabel[i])
 print("Done all in v")
 
-#%% Print count of species founded    
+# Print count of species founded    
 print(v['label'].value_counts())
 
-###########
-#%% Load audio, resample, trim, normalize and write to disk
-#for idx_row, roi_fmt in rois_fmt.iterrows():
- #   print(idx_row+1, '/', len(rois_fmt))
- #   full_path_audio = find_file(roi_fmt.fname, path_audio)[0]
- #   fname_audio = os.path.join(full_path_audio)
- #   s, fs = sound.load(fname_audio)
- #   s = sound.resample(s, fs, target_fs)
- #   s_trim = sound.trim(s, target_fs, roi_fmt.min_t, roi_fmt.max_t, pad=True)
 
- #   fname_save = os.path.join(path_save, roi_fmt.label+'_'+str(idx_row).zfill(3)) 
- #   sound.write(fname_save+'.wav', fs=target_fs, data=s_trim)
+#%%
+#Filtrar la especie
+df_rois = df[df.apply(filter_label, axis=1)]
+
+ #%%  
+# format rois to a fixed window
+rois_fmt = pd.DataFrame()
+for idx, roi in df_rois.iterrows():
+    roi_fmt = roi2windowed(wl, roi)
+    rois_fmt = pd.concat([rois_fmt,roi_fmt],ignore_index=True)
+    #rois_fmt = rois_fmt.append(roi_fmt)
+    
+rois_fmt.reset_index(inplace=True, drop=True)
+#print(rois_fmt)
+rois_fmt=rois_fmt.dropna(subset=['fname'])
+df_filt=(df[df["max_t"] <= 5])
+  
 
 #%% save data frame
+path_save_csv = "../SCRIPTS/TDS/PHYCUV/CSV/"
 rois_fmt['fname_trim'] = rois_fmt.label + '_' + rois_fmt.index.astype(str).str.zfill(3)
-rois_fmt.to_csv(path_save+'rois_details.csv', index=False)
+rois_fmt.to_csv(path_save_csv+'rois_details.csv', index=False)
 
 
-# %%
+# %%  Trimming the audios 
 import pandas as pd
 from pydub import AudioSegment
 
+path_save_aud = "../SCRIPTS/TDS/BOAALB/Audio_Trim/"
 # Read the dataframe
 df_csv = pd.read_csv('../SCRIPTS/TDS/rois_details.csv')
 df_csv['min_t'] = df_csv['min_t'].mul(1000)
@@ -139,7 +135,7 @@ for index, row in df_csv.iterrows():
     # Trim the audio
     audio_trimmed = audio[start_time:end_time]
     # Save the trimmed audio
-    audio_trimmed.export(path_save + name + ".wav", format="wav")
+    audio_trimmed.export(path_save_aud + name + ".wav", format="wav")
 
 print("All audio files have been trimmed successfully!")
 # %%
